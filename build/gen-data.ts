4082b4eb5a639d9d9fb71e6cb28e19f83bfa7217
Cotton Hou <himcotton@gmail.com>
2017-02-21 18:00:30 +0800

    building facility for generating JSON data

---
+++ build/gen-data.ts
@@ -0,0 +1,89 @@
+
+import * as file from 'fs';
+
+import * as Rx from 'rxjs';
+import fetch from 'node-fetch';
+
+import { GitHub } from '../source/script/github';
+
+import { identity, setState } from '../source/script/helper';
+
+
+const {
+    npm_package_config_github_login: login,
+    npm_package_config_github_token: token,
+
+} = process.env;
+
+
+load(login, token).toArray().subscribe(data => {
+    file.writeFile('./dist/data.json', JSON.stringify(data));
+});
+
+
+function load (user = '', token = '') {
+
+    let headers = {};
+
+    if (token) {
+        headers['Authorization'] = token;
+    }
+
+    const getJSON = function <T> (url = '') {
+        return fetch(url, {
+            headers: {
+                ...headers,
+            }
+        }).then(res => res.json<T>());
+    };
+
+    const urls = {
+        repos: `https://api.github.com/users/${ user }/repos`,
+    };
+
+    const repos$ = Rx.Observable
+        .of(urls.repos)
+        .flatMap(url => getJSON<GitHub.Repos>(url))
+        .flatMap(identity)
+        .filter(item => item.fork === true)
+        .pluck('url')
+        .flatMap((url: string) => getJSON<{parent: GitHub.Repo}>(url))
+        .map(data => data.parent)
+        .map(repo => {
+            const pseudo = <GitHub.RepoOrg>{
+                url: '',
+                description: '',
+                language: '',
+                fork: true,
+                forks_count: 0,
+                stargazers_count: 0,
+                full_name: '',
+                html_url: '',
+                name: '',
+            };
+
+            return setState(pseudo, repo);
+        })
+        .multicast(new Rx.ReplaySubject<GitHub.Repo>(Number.MAX_VALUE))
+        .refCount()
+    ;
+
+    const commits$ = repos$
+        .pluck('url')
+        .map(url => `${ url }/commits?author=${ user }`)
+        .toArray()
+        .flatMap(list =>
+            Rx.Observable.forkJoin(
+                list.map(url => getJSON<GitHub.Commits>(url))
+            )
+        )
+        .flatMap(identity)
+        .map(data => [].concat(data).map(GitHub.parseCommit))
+    ;
+
+    return Rx.Observable.zip(repos$, commits$)
+        .filter(([repo, commits]) => commits.length > 0)
+        .map(([repo, commits]) => ({repo, commits}))
+    ;
+}
+
