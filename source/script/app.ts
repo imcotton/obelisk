789dee303bc4b337c7f048ca1276697552122dd0
Cotton Hou <himcotton@gmail.com>
2017-02-21 17:54:04 +0800

    clean up

--- source/script/app.ts
+++ source/script/app.ts
@@ -7,46 +7,11 @@ import { bootWith } from './helper';
 
 
 
+
+
 function main () {
 
 
-    /*
-    return;
-
-    const { Observable } = Rx;
-
-    const user = 'imcotton';
-
-    const urls = {
-        repos: `https://api.github.com/users/${ user }/repos`,
-    };
-
-
-
-    Observable
-        .ajax(urls.repos)
-
-        .do(({xhr, response, request}) => {
-            $('#header .num').text(
-                ~~xhr.getResponseHeader('X-RateLimit-Remaining')
-            );
-        })
-
-        .flatMap(ajax => ajax.response as [{fork: Boolean}])
-        .filter(item => item.fork === true)
-        .pluck('url')
-        .take(8)
-        .flatMap(Observable.ajax)
-        .pluck('response', 'parent', 'url')
-        .map(url => url + `/commits?author=${ user }`)
-        .flatMap(Observable.ajax)
-        .pluck('response')
-        .filter((item: Array<any>) => item.length > 0)
-
-        .subscribe(console.info)
-    ;
-
-    */
 }
 
 
